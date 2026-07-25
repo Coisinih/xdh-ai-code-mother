52c0105e6a5fa155787e14c7865d1226721cc0b7
Coisinih <1549414965@qq.com>
2026-07-25 11:26:32 +0800

    1.创建全局通用布局页面 2.删除初始化默认代码 3.引入前端请求代码生成器

---
+++ xdh-ai-code-mother-frontend/openapi2ts.config.ts
@@ -0,0 +1,8 @@
+export default {
+  // request 封装路径
+  requestLibPath: "import request from '@/request'",
+  // 后端接口文档地址
+  schemaPath: 'http://localhost:8123/api/v3/api-docs',
+  // 生成代码存放的根目录的 api 下
+  serversPath: './src',
+}
